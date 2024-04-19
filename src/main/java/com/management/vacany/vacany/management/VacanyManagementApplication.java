0beaca83c816954906df31d19957338f9e67b81c
Erick Clinton <erickleite2009321@gmail.com>
2024-04-19 09:26:58 -0300

    base structure

---
+++ src/main/java/com/management/vacany/vacany/management/VacanyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.management.vacany.vacany.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacanyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacanyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/management/vacany/vacany/management/VacanyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.vacany.vacany.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacanyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
